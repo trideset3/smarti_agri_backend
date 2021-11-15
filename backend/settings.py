265f5f92a8f986ae60260c9d530384be098082d5
trideset3 <dado.matic@hotmail.com>
2021-11-16 00:14:13 +0100

    secret key bug

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*q=m(xan01eiqj4%x@4$toss5_d6j)l@xm!p=4d6y!wwes6)t('
+SECRET_KEY = 'django-insecure-*q=m(xan01eiqj4%x@4$toss5_d6j)l@xm!p=4d6y!wwes6)t('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
