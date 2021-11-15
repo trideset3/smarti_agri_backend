9203f010aa0ab53a2ce8da9596bab892816b5ca0
trideset3 <dado.matic@hotmail.com>
2021-11-16 00:09:28 +0100

    gdal bug

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','cryptic-shelf-86597.herokuapp.com']
 
 
 # Application definition
